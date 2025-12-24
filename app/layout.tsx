b9900b5e3b1772c6d6e0b1cb3da675f045feb01b
cronin <58665249+Cronin@users.noreply.github.com>
2025-12-24 16:28:20 +0800

    fix: Update GA4 tracking measurement ID

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,7 +104,24 @@ export default function RootLayout({
           `,
         }}
       />
-</body>
+
+        <!-- Google Analytics -->
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-E013LYXKC9"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-E013LYXKC9', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
+
+      </body>
     </html>
   );
 }
